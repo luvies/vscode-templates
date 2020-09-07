6fde07e57b12635fca03988cb24d93c991ec164e
Luna <luna@luvies.io>
2020-09-07 20:08:42 +0100

    Fixed deno deps

--- deps.ts
+++ deps.ts
@@ -2,9 +2,9 @@ export {
   ArgParsingOptions,
   Args,
   parse,
-} from "https://deno.land/std@0.57.0/flags/mod.ts";
+} from "https://deno.land/std@0.68.0/flags/mod.ts";
 export {
   dirname,
   fromFileUrl,
   join,
-} from "https://deno.land/std@0.57.0/path/mod.ts";
+} from "https://deno.land/std@0.68.0/path/mod.ts";
